33cf30aa9946994255664030a464f8fa82bf14d4
Gloria <gloris_flaque@hotmail.com>
2019-02-06 14:19:54 +0100

    Image Picker

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,31 +1,63 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key, this.user}) : super(key: key);
   final FirebaseUser user;
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  File image;
+
+//  To use Gallery or File Manager to pick Image
+//  Comment Line No. 19 and uncomment Line number 20
+  picker() async {
+    print('Picker is called');
+    File img = await ImagePicker.pickImage(source: ImageSource.camera);
+//    File img = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (img != null) {
+      image = img;
+      setState(() {});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    var images;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home ${user.email}'),
+        title: Text('Home'),
       ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance
-            .collection('users')
-            .document(user.uid)
-            .snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            return checkRole(snapshot.data);
-          }
-          return LinearProgressIndicator();
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (c, position) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).pop(position);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(
+                color: Colors.blue,
+                child: Center(child: image == null
+                    ? new Text('No Image to Show ')
+                    : new Image.file(image),),
+              ),
+            ),
+          );
         },
+        itemCount: images.length,
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: picker,
+        child: new Icon(Icons.camera_alt),
       ),
     );
   }
@@ -51,4 +83,11 @@ class Home extends StatelessWidget {
   Center userPage(DocumentSnapshot snapshot) {
     return Center(child: Text(snapshot.data['name']));
   }
+
 }
+
+
+
+
+
+
